c72b2867d79f1e85736b2e109ef479edfd1498c7
yashkin <35468656+yashkin@users.noreply.github.com>
2022-10-11 01:57:34 +0300

    rest bug fix

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let number = 0;
-   function increment() {
-       number++;
-       document.getElementById("number").innerHTML = number;
-   }
-   function decrement() {
-       number--;
-       document.getElementById("number").innerHTML = number;
-   }
-   function reset() {
-       let number = 0;
-       document.getElementById("number").innerHTML = number;
-   }
+function increment() {
+    number++;
+    document.getElementById("number").innerHTML = number;
+}
+function decrement() {
+    number--;
+    document.getElementById("number").innerHTML = number;
+}
+function reset() {
+    document.getElementById("number").innerHTML = number;
+}
